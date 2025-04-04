5d5003f2793169e709d79a2d90c361468d735531
Dhashwathi M B <dhashwathi.mb2021ai@sece.ac.in>
2025-04-04 16:05:40 +0530

    ADD- TC002 Dashboard functionality is validated

--- tests/tc001_loginPage.spec.js
+++ tests/tc001_loginPage.spec.js
@@ -14,16 +14,13 @@ test.describe("Testing login page functionality",()=>{
     test("Testing login page with invalid credentials",async({page})=>{
         await page.goto("https://pyramidcore.pyramidci.com/security/PCILoginNew.aspx");
         await page.locator("#pydLogin_txtUserid").fill("Dhashwathi.M@celsiortech.com");
-        await page.locator("#pydLogin_txtUserPwd").fill("#######");
+        await page.locator("#pydLogin_txtUserPwd").fill("#########");
         await page.click('#pydLogin_btnLogin');
         await expect(page.locator("#pydLogin_lblMsg")).toHaveText("* User not found Or User/Password not matching");
     })
 
     test("Testing login page forget password",async({page})=>{
         await page.goto("https://pyramidcore.pyramidci.com/security/PCILoginNew.aspx");
-        await page.locator("#pydLogin_txtUserid").fill("Dhashwathi.M@celsiortech.com");
-        await page.click('.txt1');
-        const frame = await page.frameLocator("//iframe[@src='PCIForget.aspx']");
         await frame.locator("(//input[@id='txtemail'])[1]").fill("Dhashwathi@celsiortech.com")
         await expect(frame.locator("#btngo")).toBeEnabled();
     })
---
+++ tests/tc002_dashboard.spec.js
@@ -0,0 +1,38 @@
+import {test,expect} from '@playwright/test'
+
+test.describe("Testing Dashboard functionality",()=>{
+    test("Testing dashboard",async({page})=>{
+        await page.goto("https://pyramidcore.pyramidci.com/security/PCILoginNew.aspx");
+        await page.locator("#pydLogin_txtUserid").fill("Dhashwathi.M@celsiortech.com");
+        await page.locator("#pydLogin_txtUserPwd").fill("Dhash@12345");
+        await page.click('#pydLogin_btnLogin');
+        await page.waitForLoadState('networkidle'); 
+        const contentsFrame = page.frame({ name: "contents" });
+        await contentsFrame.locator("#PCIMenut0").click();
+        const mainFrame = page.frame({ name: "main" });
+        const checkBox= mainFrame.locator("#rdSelf");
+        //dashboard is enabled
+        await expect(checkBox).toBeChecked(); 
+        await mainFrame.click("#lnkAttendance");
+        const days=await mainFrame.locator("#lnkAttendance").innerText();
+        const nday=parseInt(days);
+        //console.log(nday);
+        await mainFrame.waitForSelector("//tr[@class='griditem']");
+        await mainFrame.waitForSelector("//tr[@class='gridalternetitem']")
+        const rows= mainFrame.locator("//tr[@class='griditem']")
+        const arows=mainFrame.locator("//tr[@class='gridalternetitem']")
+        const rcount=await rows.count();
+        const acount=await arows.count();
+        //assert
+        await expect(rcount+acount).toBe(nday);
+        await mainFrame.locator("#rdTeam").click();
+        await mainFrame.waitForSelector("#lblTodayDueTickets")
+        //assert
+        await expect(mainFrame.locator("#lblTodayDueTickets")).toBeVisible();
+        const topframe=page.frame({ name: "top" });
+        await topframe.locator(".logout").click();
+        //assert
+        await expect(page.locator('#pydLogin_lblMsg')).toHaveText("You have been logged out successfully.");
+       })
+
+})
